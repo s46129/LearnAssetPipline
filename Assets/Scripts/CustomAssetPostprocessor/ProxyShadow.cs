02ed85ba4cf7909eedd9610653cc1bb4c50ab62d
Rex <s46129@yahoo.com.tw>
2023-07-25 14:21:21 +0800

    feat : Auto Add rigibody and collider

--- Assets/Scripts/CustomAssetPostprocessor/ProxyShadow.cs
+++ Assets/Scripts/CustomAssetPostprocessor/ProxyShadow.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 
@@ -5,37 +6,40 @@ public class ProxyShadow : AssetPostprocessor
 {
     public void OnPostprocessModel(GameObject gameObject)
     {
-        var children = gameObject.transform.GetComponentsInChildren<GameObject>();
-        if (children == null || children.Length == 0)
+        List<GameObject> removeObject = new List<GameObject>();
+        foreach (Transform childTransform in gameObject.transform)
         {
-            return;
-        }
-
-        var componentsInChildren = gameObject.GetComponentsInChildren<MeshRenderer>();
-
-        foreach (var meshRenderer in componentsInChildren)
-        {
-            if (meshRenderer.transform.name.EndsWith(".shadow"))
+            Debug.Log(childTransform.name);
+            if (childTransform.name.EndsWith(".collider"))
             {
+                var collider = gameObject.AddComponent<MeshCollider>();
+                var meshFilter = childTransform.gameObject.GetComponent<MeshFilter>();
+                if (meshFilter)
+                {
+                    collider.sharedMesh = meshFilter.sharedMesh;
+                }
+
+                gameObject.AddComponent<Rigidbody>();
+                var meshRenderer = childTransform.GetComponent<MeshRenderer>();
+                meshRenderer.enabled = false;
+                removeObject.Add(childTransform.gameObject);
+                continue;
+            }
+
+            if (childTransform.name.EndsWith(".shadow"))
+            {
+                var meshRenderer = childTransform.GetComponent<MeshRenderer>();
                 meshRenderer.shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.ShadowsOnly;
                 meshRenderer.receiveShadows = true;
             }
             else
             {
+                var meshRenderer = childTransform.GetComponent<MeshRenderer>();
                 meshRenderer.shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.Off;
                 meshRenderer.receiveShadows = false;
             }
         }
-        
-		////嘗試新增MeshCollider，目前還不成功，要再試一下
-        // foreach (var child in children)
-        // {
-        //     if (child.name.EndsWith(".collider"))
-        //     {
-        //         var addComponent = child.AddComponent<MeshCollider>();
-        //         addComponent.sharedMesh = child.GetComponent<MeshFilter>().sharedMesh;
-        //         Object.DestroyImmediate(child);
-        //     }
-        // }
+
+        removeObject.ForEach(Object.DestroyImmediate);
     }
 }
